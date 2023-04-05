c6edce795e79e1c9bb3ab08142e47a9d007ef29d
Charlie Mourant <Charlie.mourant@gocity.com>
2023-04-05 20:43:16 +0100

    feat: update schema and seed to use posgresql and seat a whole 25 seats

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,23 +2,25 @@ import { prisma } from "~/server/db";
 import * as process from "process";
 
 async function main() {
-  const seats = [
-    { id: "cl9ebqhxk00003b600tymydho", seatNumber: "1" },
-    {
-      id: "cl9ebqhxk00003b600tymyddo",
-      seatNumber: "2",
-    },
-  ] as const;
-  await prisma.seat.upsert({
-    where: {
-      id: seats[0].id,
-    },
-    create: {
-      id: seats[0].id,
-      seatNumber: seats[0].seatNumber,
-    },
-    update: {},
-  });
+  const seatNumbers = Array.from({ length: 25 }, (_, index) =>
+    (index + 1).toString()
+  );
+
+  const seatPromises = seatNumbers.map(
+    async (seatNumber) =>
+      await prisma.seat.upsert({
+        where: {
+          seatNumber: seatNumber,
+        },
+        create: {
+          seatNumber: seatNumber,
+        },
+        update: {},
+      })
+  );
+  for (const seatPromise of seatPromises) {
+    await seatPromise;
+  }
 }
 
 main()
